efb75da59bc4466658ed18d06cfcb1968d8850b5
Edward Tran <trang5@student.douglascollege.ca>
2024-03-11 12:56:17 -0700

    changed map to manhattan map

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import * as CANNON from 'cannon-es';
-import CannonDebugger from 'cannon-es-debugger';
+import * as CANNON from "cannon-es";
+import CannonDebugger from "cannon-es-debugger";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { VRButton } from "three/addons/webxr/VRButton";
 import { OBJLoader } from "three/addons/loaders/OBJLoader";
@@ -9,7 +9,12 @@ function init() {}
 
 // Set up the window
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  90,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -51,11 +56,12 @@ busLoader.load(
 //map
 const mapLoader2 = new GLTFLoader();
 let cityMap;
-mapLoader2.load("citymap-scaled-0.2.glb", function (gltf) {
+mapLoader2.load("manhattan-map.glb", function (gltf) {
   cityMap = gltf.scene;
   scene.add(cityMap);
   if (cityMap) {
-    cityMap.position.set(0, 0, 0);
+    cityMap.scale.set(0.4, 0.3, 0.4);
+    cityMap.position.set(35, -1, 0);
   }
 });
 
@@ -83,11 +89,11 @@ const physicsWorld = new CANNON.World({
 });
 
 // Add a ground body
-const groundMaterial = new CANNON.Material('ground');
+const groundMaterial = new CANNON.Material("ground");
 const groundBody = new CANNON.Body({
   type: CANNON.Body.STATIC,
   shape: new CANNON.Plane(),
-  material: groundMaterial
+  material: groundMaterial,
 });
 groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 groundBody.position.set(0, 0, 0);
@@ -96,7 +102,7 @@ physicsWorld.addBody(groundBody);
 // For testing only
 const prismBody = new CANNON.Body({
   mass: 0,
-  shape: new CANNON.Box(new CANNON.Vec3(1, 5, 1))
+  shape: new CANNON.Box(new CANNON.Vec3(1, 5, 1)),
 });
 prismBody.position.set(10, 0, -10);
 physicsWorld.addBody(prismBody);
@@ -114,7 +120,7 @@ const vehicle = new CANNON.RigidVehicle({
 // Add wheels to the vehicle
 const mass = 1;
 const wheelShape = new CANNON.Sphere(0.5);
-const wheelMaterial = new CANNON.Material('wheel');
+const wheelMaterial = new CANNON.Material("wheel");
 const down = new CANNON.Vec3(0, -1, 0);
 const wheelBody1 = new CANNON.Body({ mass, material: wheelMaterial });
 wheelBody1.addShape(wheelShape);
@@ -152,7 +158,7 @@ vehicle.addWheel({
 });
 vehicle.wheelBodies.forEach((wheelBody) => {
   // Some damping to not spin wheels too fast
-  wheelBody.angularDamping = 0.9
+  wheelBody.angularDamping = 0.9;
 });
 
 vehicle.addToWorld(physicsWorld);
@@ -172,56 +178,55 @@ physicsWorld.addContactMaterial(wheel_ground);
 // Add force on keydown
 const maxSpeed = 30;
 const maxForce = 20;
-document.addEventListener('keydown', (event) => {
-const maxSteerVal = Math.PI / 8;
+document.addEventListener("keydown", (event) => {
+  const maxSteerVal = Math.PI / 8;
 
+  switch (event.key) {
+    case "w":
+      vehicle.setWheelForce(maxForce, 2);
+      vehicle.setWheelForce(-maxForce, 3);
+      break;
 
-switch (event.key) {
-  case 'w':
-  vehicle.setWheelForce(maxForce, 2);
-  vehicle.setWheelForce(-maxForce, 3);
-  break;
+    case "s":
+      vehicle.setWheelForce(-maxForce / 2, 2);
+      vehicle.setWheelForce(maxForce / 2, 3);
+      break;
 
-  case 's':
-  vehicle.setWheelForce(-maxForce / 2, 2);
-  vehicle.setWheelForce(maxForce / 2, 3);
-  break;
+    case "a":
+      vehicle.setSteeringValue(maxSteerVal, 0);
+      vehicle.setSteeringValue(maxSteerVal, 1);
+      break;
 
-  case 'a':
-  vehicle.setSteeringValue(maxSteerVal, 0);
-  vehicle.setSteeringValue(maxSteerVal, 1);
-  break;
-
-  case 'd':
-  vehicle.setSteeringValue(-maxSteerVal, 0);
-  vehicle.setSteeringValue(-maxSteerVal, 1);
-  break;
-}
-})
+    case "d":
+      vehicle.setSteeringValue(-maxSteerVal, 0);
+      vehicle.setSteeringValue(-maxSteerVal, 1);
+      break;
+  }
+});
 // Reset force on keyup
-document.addEventListener('keyup', (event) => {
-switch (event.key) {
-  case 'w':
-  vehicle.setWheelForce(0, 2);
-  vehicle.setWheelForce(0, 3);
-  break;
+document.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "w":
+      vehicle.setWheelForce(0, 2);
+      vehicle.setWheelForce(0, 3);
+      break;
 
-  case 's':
-  vehicle.setWheelForce(0, 2);
-  vehicle.setWheelForce(0, 3);
-  break;
+    case "s":
+      vehicle.setWheelForce(0, 2);
+      vehicle.setWheelForce(0, 3);
+      break;
 
-  case 'a':
-  vehicle.setSteeringValue(0, 0);
-  vehicle.setSteeringValue(0, 1);
-  break;
+    case "a":
+      vehicle.setSteeringValue(0, 0);
+      vehicle.setSteeringValue(0, 1);
+      break;
 
-  case 'd':
-  vehicle.setSteeringValue(0, 0);
-  vehicle.setSteeringValue(0, 1);
-  break;
-}
-})
+    case "d":
+      vehicle.setSteeringValue(0, 0);
+      vehicle.setSteeringValue(0, 1);
+      break;
+  }
+});
 
 const cannonDebugger = new CannonDebugger(scene, physicsWorld, {});
 // Animation loop
@@ -234,10 +239,18 @@ function animate() {
       // Update the bus's position and rotation
       busModel.position.x = vehicle.chassisBody.position.x;
       busModel.position.z = vehicle.chassisBody.position.z;
-      busModel.rotation.y = angle.y - Math.PI/2;
+      busModel.rotation.y = angle.y - Math.PI / 2;
     }
-    camera.position.set(vehicle.chassisBody.position.x + 6 * Math.sin(angle.y), 10, vehicle.chassisBody.position.z + 6 * Math.cos(angle.y));
-    camera.lookAt(vehicle.chassisBody.position.x, 0, vehicle.chassisBody.position.z);
+    camera.position.set(
+      vehicle.chassisBody.position.x + 5 * Math.sin(angle.y),
+      30,
+      vehicle.chassisBody.position.z + 5 * Math.cos(angle.y)
+    );
+    camera.lookAt(
+      vehicle.chassisBody.position.x,
+      0,
+      vehicle.chassisBody.position.z
+    );
     //console.log(vehicle.getWheelSpeed(2));
     renderer.render(scene, camera);
   });
